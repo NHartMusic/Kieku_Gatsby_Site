f0f271b7ce831644273cca1453f88a4c22ba24a8
Nick Hart <nhartmusic@gmail.com>
2020-10-26 15:41:22 +0100

    installed a lot of shit, still trying to figure out this whole gatsby/ts/styled components setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kieku Teknologia`,
+    description: `Basic Gatsby site for Kieku Records new tech division, spearheaded by Nick Hart`,
+    author: `Nick Hart`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,31 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+import { Cyans, Greyscale } from '../UI/Colors'
+
+const SiteHeader = styled.div`
+  display: flex;
+  margin: 0 auto;
+  background-color: ${ Cyans[3]};
+  padding: 20px;
+`
+
+const SiteTitle: JSX.Element = styled.h1`
+  display: flex;
+  margin: 0 auto;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <SiteHeader>
+    <SiteTitle>
+      <Link>
+        {siteTitle}
+      </Link>
+    </SiteTitle>
+
+
+  </SiteHeader >
 )
 
 Header.propTypes = {
@@ -36,7 +33,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `Kieku`,
 }
 
 export default Header
